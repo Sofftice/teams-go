254e10a3adc2597fe29b142025e5e0802e55a433
JXSnack <jx.snack@gmail.com>
2026-01-17 01:12:27 +0100

    rework login page

--- page/login.go
+++ page/login.go
@@ -10,11 +10,20 @@ func DisplayLoginPage(window *qt.QMainWindow) {
 
 	NewCenterHLayout(widget, func(layout *qt.QHBoxLayout) {
 		NewCenterVLayout2(layout.QBoxLayout, func(layout *qt.QVBoxLayout) {
-			text := qt.NewQLabel3("Test")
-			layout.AddWidget(text.QWidget)
+			title := widgets.NewHeader("Welcome to Teams")
+			layout.AddWidget(title.QWidget)
 
-			text2 := widgets.NewPrimaryButton("Sign in")
-			layout.AddWidget(text2.QWidget)
+			subtitle := widgets.NewLabel("Chat and collaborate in one app.")
+			subtitle.SetAlignment(qt.AlignCenter)
+			layout.AddWidget(subtitle.QWidget)
+
+			//image := widgets.NewImage("img/person.png").Constrain(200, 200)
+			//layout.AddWidget(image.QWidget)
+
+			layout.AddSpacing(50 + 200)
+
+			signIn := widgets.NewPrimaryButton("Sign in")
+			layout.AddWidget(signIn.QWidget)
 		})
 	})
 }
